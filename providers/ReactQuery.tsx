cf19be71eb06fa01d4df4718066fa05d29db7079
sabbirlog <mridhasabbir5@gmail.com>
2023-11-30 15:06:42 +0600

    feat: setup query providers

---
+++ providers/ReactQuery.tsx
@@ -0,0 +1,18 @@
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query'
+
+const client = new QueryClient();
+
+const ReactQueryProvider = ({ children }: {
+    children: React.ReactNode
+}) => {
+    return (
+        <QueryClientProvider client={client}>
+            {children}
+        </QueryClientProvider>
+    )
+}
+
+export default ReactQueryProvider
